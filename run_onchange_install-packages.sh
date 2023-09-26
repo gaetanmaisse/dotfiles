c20842a1c254c0f5973b16f37fd7dc5d49d25411
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-09-26 16:20:41 +0200

    Run install script only on laptop running MacO

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# if we're not on a Mac, don't continue
+if [ "$(uname)" != "Darwin" ]; then
+  echo "Not on a Mac, exiting..."
+  exit 0
+fi
+
 # if `brew` is not installed then install it
 if test ! $(which brew); then
   echo "Installing homebrew..."
