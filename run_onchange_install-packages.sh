a063e5ba7fdd031a226e14e12a85cc87feedfa7c
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-09-26 09:23:10 +0200

    Create a simple install-packages script

---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# if `brew` is not installed then install it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
